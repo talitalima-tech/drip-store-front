cda2d74dda52671ba4e6b304d5217afc232c65a0
Talita Lima <talita.lima.tech@gmail.com>
2026-02-16 12:36:01 -0300

    feat: implementação inicial da home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
-import ProductListing from '../../components/ProductListing/ProductListing';
+import Section from "../../components/Section/Section";
+import Layout from "../../Layouts/Layout";
+import Gallery from "../../components/Gallery/Gallery";
+import ProductListing from "../../components/ProductListing/ProductListing";
 
-export default function HomePage() {
+const HomePage = () => {
   return (
-    <div style={{ padding: '40px 104px' }}>
-      <h1 style={{ fontSize: '32px', color: '#1F1F1F', marginBottom: '24px' }}>Bem-vindo à Drip Store</h1>
-      <ProductListing />
-    </div>
+    <Layout>
+
+      {/* SLIDE */}
+      <Gallery
+        images={[
+          { src: "/home-slide-1.jpeg" },
+          { src: "/home-slide-2.jpeg" },
+          { src: "/home-slide-3.jpeg" }
+        ]}
+        width="1440px"
+        height="681px"
+      />
+
+      {/* COLEÇÕES */}
+      <Section title="Coleções em destaque" titleAlign="center">
+        <div style={{
+          display: "flex",
+          gap: "20px",
+          justifyContent: "center"
+        }}>
+          <img src="/collection-1.png" style={{ borderRadius: "4px" }} />
+          <img src="/collection-2.png" style={{ borderRadius: "4px" }} />
+          <img src="/collection-3.png" style={{ borderRadius: "4px" }} />
+        </div>
+      </Section>
+
+      {/* PRODUTOS */}
+      <Section title="Produtos em alta">
+        <ProductListing
+          products={[
+            {
+              name: "Produto 1",
+              image: "/product-thumb-1.png",
+              price: 200,
+              priceDiscount: 149.9
+            },
+            {
+              name: "Produto 2",
+              image: "/product-thumb-2.png",
+              price: 49.9
+            },
+            {
+              name: "Produto 3",
+              image: "/product-thumb-3.png",
+              price: 99.9
+            },
+            {
+              name: "Produto 4",
+              image: "/product-thumb-4.png",
+              price: 199.9
+            },
+            {
+              name: "Produto 5",
+              image: "/product-thumb-1.png",
+              price: 120
+            },
+            {
+              name: "Produto 6",
+              image: "/product-thumb-2.png",
+              price: 89.9
+            },
+            {
+              name: "Produto 7",
+              image: "/product-thumb-3.png",
+              price: 300,
+              priceDiscount: 249.9
+            },
+            {
+              name: "Produto 8",
+              image: "/product-thumb-4.png",
+              price: 59.9
+            }
+          ]}
+        />
+      </Section>
+
+    </Layout>
   );
-}
+};
+
+export default HomePage;

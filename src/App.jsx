309b075d651155f129d1b9142c4539ac5eee99be
Talita Lima <talita.lima.tech@gmail.com>
2026-02-17 15:01:37 -0300

    chore: adiciona a rota /produtos no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import ProductListingPage from "./pages/ProductListingPage/ProductListingPage";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/produtos" element={<ProductListingPage />} />
       </Routes>
     </BrowserRouter>
   );

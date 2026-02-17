097291e46a7be36c1df7cbb4846dd9916229eac5
Talita Lima <talita.lima.tech@gmail.com>
2026-02-17 14:38:56 -0300

    mudanças no products e home slides

--- src/data/db.js
+++ src/data/db.js
@@ -13,18 +13,16 @@ export const CATEGORIES = [
 ];
 
 export const PRODUCTS = [
-  { id: 1, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 200, priceDiscount: 149.9, category: "Tênis", tag: "30% OFF" },
-  { id: 2, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 200, priceDiscount: 149.9, category: "Tênis", tag: "30% OFF" },
-  { id: 3, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 200, category: "Tênis" },
-  { id: 4, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 200, category: "Tênis" },
-  { id: 5, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 200, priceDiscount: 149.9, category: "Tênis" },
-  { id: 6, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 250, category: "Tênis" },
-  { id: 7, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 200, priceDiscount: 149.9, category: "Tênis" },
-  { id: 8, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", price: 300, category: "Tênis" }
+  { id: 1, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png",priceDiscount: 100, price: 200, category: "Tênis", tag: "30% OFF" },
+  { id: 2, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", priceDiscount: 100, price: 200, category: "Tênis", tag: "30% OFF" },
+  { id: 3, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", priceDiscount: 100, price: 200, category: "Tênis" },
+  { id: 4, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", priceDiscount: 100, price: 200, category: "Tênis" },
+  { id: 5, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", priceDiscount: 100, price: 200, category: "Tênis" },
+  { id: 6, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", priceDiscount: 100, price: 200, category: "Tênis" },
+  { id: 7, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png", priceDiscount: 100, price: 200, category: "Tênis" },
+  { id: 8, name: "K-Swiss V8 - Masculino", image: "product-placeholder.png",priceDiscount: 100, price: 200, category: "Tênis" }
 ];
 
 export const HOME_SLIDES = [
-  { src: "/home-slide-1.jpeg" },
-  { src: "/home-slide-2.jpeg" },
-  { src: "/home-slide-3.jpeg" }
+  { src: "/banner-nike.png", title: "Queima de estoque Nike", subTitle: "Melhores ofertas personalizadas", description: "Consequat culpa exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur." }
 ];
